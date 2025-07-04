394daa1fca674d7192197336ca31e4da1ee6b86a
varelsaurus <mhdvarels@gmail.com>
2025-07-04 08:37:17 +0700

    Membuat logika untuk load gambar secara bersamaan

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const sesiwelcome = document.getElementById('welcome-section')
+const gambarawal = document.getElementById('gambarwelcome')
+
+gambarawal.onload = function() {
+    sesiwelcome.style.visibility = 'visible';
+}
