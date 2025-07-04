6a3fb45b555ed32cf0730dc5e841078ef9d9b231
varelsaurus <mhdvarels@gmail.com>
2025-07-04 08:47:32 +0700

    Membuat logika greeting

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-const sesiwelcome = document.getElementById('welcome-section')
-const gambarawal = document.getElementById('gambarwelcome')
+const sesiwelcome = document.getElementById('welcome-section');
+const gambarawal = document.getElementById('gambarwelcome');
+const salam = document.getElementById('salaman');
 
 gambarawal.onload = function() {
     sesiwelcome.style.visibility = 'visible';
+} 
+
+window.onload = function() {
+    let namauser = prompt('Siapa nama kamu?');
+    salam.textContent = `Hi ${namauser}, Welcome To Website`;
 }
